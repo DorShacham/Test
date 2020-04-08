d482e29f2cbef17df6d1b73f65e7c80f51b8f98b
Dor-Hay Shacham <dor.shacham@gmail.com>
2020-04-08 12:36:37 +0300

    main was added

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <assert.h>
+
+int main()
+{
+    int x = 0;
+    char str[100] = "";
+    scanf("%d %s",&x, str );
+    printf("Hello World!\n%d.\t%s\n",x ,str);
+    return 0;
+}
